6f6a1af75a1dbc04927c9eb6ad43dc5b6ba94a1e
longxuan <admin@remiliascarlet.com>
2023-07-03 19:59:07 +0800

    partial settings implmenet

--- Zast.Player.CUI/Scripts/Scenes/SettingScene.cs
+++ Zast.Player.CUI/Scripts/Scenes/SettingScene.cs
@@ -1,7 +1,14 @@
 namespace Zast.Player.CUI.Scripts.Scenes
 {
-    public class SettingScene
+    public class SettingScene : IMenuItem
     {
-        
+        public Type Category => typeof(MainMenuScene);
+
+        public string Name => "设置...";
+
+        public ValueTask<IScript> Show(IScript prev, ScriptContext context, CancellationToken cancellationToken)
+        {
+            return ValueTask.FromResult(prev);
+        }
     }
 }
--- Zast.Player.CUI/ZastCuiSetting.cs
+++ Zast.Player.CUI/ZastCuiSetting.cs
@@ -4,6 +4,17 @@ namespace Zast.Player.CUI
 {
     public class ZastCuiSetting
     {
-        
+        public static List<(Action<bool, ZastCuiSetting>, string)> GetSettingItems(ZastCuiSetting setting)
+        {
+            return new()
+            {
+                ((v, s) => s.EnabeldWhisper = v, "启用Whisper进行实时直播语音识别 [red](不推荐，不稳定，效果差)[/]"),
+                ((v, s) => s.EnabledAudio = v, "播放直播语音流 [grey]使用BASS[/]")
+            };
+        }
+
+        public bool EnabeldWhisper { get; set; }
+
+        public bool EnabledAudio { get; set; }
     }
 }
