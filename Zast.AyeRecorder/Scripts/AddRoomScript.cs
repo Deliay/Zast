979c805ce57ced56e8f11a69297f5eecaac3d030
ZeroAsh <admin@remiliascarlet.com>
2023-07-08 01:40:15 +0800

    complete recording implemention

---
+++ Zast.AyeRecorder/Scripts/AddRoomScript.cs
@@ -0,0 +1,53 @@
+﻿using Mikibot.Crawler.Http.Bilibili;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zast.AyeRecorder.Config;
+using Zast.BuildingBlocks.Scripts;
+
+namespace Zast.AyeRecorder.Scripts
+{
+    public class AddRoomScript : IScript
+    {
+        private readonly RecordConfigRepository configRepository;
+        private readonly BiliLiveCrawler crawler;
+
+        public AddRoomScript(RecordConfigRepository configRepository, BiliLiveCrawler crawler)
+        {
+            this.configRepository = configRepository;
+            this.crawler = crawler;
+        }
+
+        public string Name => "添加录制";
+
+        public async ValueTask<IScript> Show(IScript prev, ScriptContext context, CancellationToken cancellationToken)
+        {
+            var config = await configRepository.Load(cancellationToken) ?? RecordConfig.Default();
+
+            var args = Environment.GetCommandLineArgs();
+
+            if (args.Length < 3)
+            {
+                AnsiConsole.MarkupLine("[red]必须输入房间号[/]");
+                return default!;
+            }
+
+            var rawRoomId = args[2];
+            if (!int.TryParse(rawRoomId, out var roomId))
+            {
+                AnsiConsole.MarkupLine($"{rawRoomId} 不是合法的房间号");
+            }
+
+            _ = await crawler.GetLiveRoomInfo(roomId, cancellationToken);
+
+            config.RoomIds.Add(roomId);
+
+            await configRepository.Save(config, cancellationToken);
+            AnsiConsole.MarkupLine($"[yellow]{roomId}[/] 添加成功");
+            return default!;
+        }
+    }
+}
---
+++ Zast.AyeRecorder/Scripts/RemoveRoomScript.cs
@@ -0,0 +1,50 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zast.AyeRecorder.Config;
+using Zast.BuildingBlocks.Scripts;
+
+namespace Zast.AyeRecorder.Scripts
+{
+    internal class RemoveRoomScript : IScript
+    {
+        private readonly RecordConfigRepository configRepository;
+
+        public RemoveRoomScript(RecordConfigRepository configRepository)
+        {
+            this.configRepository = configRepository;
+        }
+
+        public string Name => "移除房间";
+
+        public async ValueTask<IScript> Show(IScript prev, ScriptContext context, CancellationToken cancellationToken)
+        {
+            var config = await configRepository.Load(cancellationToken) ?? RecordConfig.Default();
+
+            var args = Environment.GetCommandLineArgs();
+
+            if (args.Length < 3)
+            {
+                AnsiConsole.MarkupLine("[red]必须输入房间号[/]");
+                return default!;
+            }
+
+            var rawRoomId = args[2];
+            if (!int.TryParse(rawRoomId, out var roomId))
+            {
+                AnsiConsole.MarkupLine($"{rawRoomId} 不是合法的房间号");
+            }
+
+
+            config.RoomIds.Remove(roomId);
+
+            await configRepository.Save(config, cancellationToken);
+
+            AnsiConsole.MarkupLine($"[yellow]{roomId}[/] 移除成功");
+            return default!;
+        }
+    }
+}
